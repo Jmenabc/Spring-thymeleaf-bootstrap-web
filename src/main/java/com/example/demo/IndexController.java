5bbc7fae788519232e332b788311823b53dcaf04
DESKTOP-HGE6DGN\javi <csi22-jmennza@colegioaltair.net>
2022-11-21 13:43:08 +0100

    Añadido bootstrap

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -4,24 +4,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-	@GetMapping("/home")
-	public String index2(Model model) {
-		String nombre = "Javier";
-		model.addAttribute("nombre", nombre == null ? "Nombre no disponible" : "Javier");
+	@RequestMapping("/")
+	public String index(Model model) {
+		model.addAttribute("mundo", "Mundo");
 		return "index";
+	}
+
+	@RequestMapping("/HolaMundo")
+	public String HolaMundo(Model model) {
+		model.addAttribute("username", "Javier Mena-Bernal Carranza");
+		return "holamundo";
 	}
 
 	@Autowired
 	gestionAlmacen gestorTorre;
 
-	@GetMapping("/listaproductos1")
-	public String index(Model modelo) {
-		modelo.addAttribute("listaproductos1", gestorTorre.listarTodosProductos1());
-		return "index";
+	@RequestMapping("/ListaPedidos")
+	public String ListaPedidos(Model model) {
+		model.addAttribute("lista", gestorTorre.listarTodosProductos1());
+		return "listapedidos";
 	}
 
 }
